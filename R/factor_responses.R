8182191370c9f33cf7f0dc6070fba150f6de8e3d
Christian Million <christianmillion93@gmail.com>
2018-09-25 15:38:52 -0700

    Add factor_cols to get_factor

--- R/factor_responses.R
+++ R/factor_responses.R
@@ -2,34 +2,41 @@
 
 ## A couple thoughts:
   # Function to factor columns and vary levels
+    #For factor_cols add "add level" argument to add 1 level to end of unique()
   # Then a get_factor_question function with factor_col embedded
 
 library(dplyr)
 survey <- read.csv("data/clean/svdg_clean.csv", stringsAsFactors = FALSE)
+schema <- read.csv("data/clean/svdg_schema.csv", stringsAsFactors = FALSE)
 factor_df <- survey %>%
-              select(id, member_length, ret_feel_connected, ret_skills_recognized, non_derby_participation)
+              select(id, 
+                     member_length, 
+                     ret_feel_connected, 
+                     ret_skills_recognized, 
+                     non_derby_participation)
 
 # Function to factor columns with levels
 factor_cols <- function(data, ..., lvls){
 
   df <- data
-  cols <- sapply(substitute(list(...)),deparse)[-1]
+  colnames <- quos(...)
+  cols <- as.character(sapply(colnames, rlang::quo_get_expr))
   
-  if (missing(lvls)){
+    if (missing(lvls)){
+      
+      y <- Map(unique, df[cols])
+      ind <- lapply(y, order)
+      lvls_lst <- Map(function(x, i)unique(as.character(x)[i]), y, ind)
+      
+    } else if(length(lvls) == length(cols)){
+      
+      lvls_lst <- lvls
+      
+    } else {
+      
+     stop("# of Levels Does Not Equal Number of Columns")
     
-    y <- Map(unique, df[cols])
-    ind <- sapply(y, order)
-    lvls_lst <- Map(function(x, i)unique(as.character(x)[i]), y, ind)
-    
-  } else if(length(lvls) == length(cols)){
-    
-    lvls_lst <- lvls
-    
-  } else {
-    
-   stop("# of Levels Does Not Equal Number of Columns")
-  
-  }
+    }
   
   df[cols] <- Map(factor, x = df[cols], levels = lvls_lst)
   
@@ -42,12 +49,19 @@ factor_cols <- function(data, ..., lvls){
 #test_lvls <-  list(c("0 - 6 months","6 months -  1 year","1 - 2 years","2 - 3 years","5+ years","Butt"),c(response_scale("agree"), "B"))
 #test <- factor_cols(data = factor_df, member_length, ret_skills_recognized)
 
-
-
-get_factors <- function(...){
-    factor_response_qs <- schema$q_varname[schema$q_type %in% c("factor", "ordered", "other_factor")]
-    
+get_factors <- function(..., factor_lvls = NULL){
+    factor_response_qs <- schema$q_varname[schema$q_type %in% c("factor", 
+                                                                "ordered", 
+                                                                "other_factor")]
     columns <- quos(...)
+    
+    if(is.null(factor_lvls)){
+      fctr <- NULL
+      }else if(is.logical(factor_lvls)){
+      fctr <- TRUE
+      } else if(is.list(factor_lvls)){
+      fctr <- "okay"
+      }
     
     if(length(columns) == 0){
       
@@ -59,7 +73,16 @@ get_factors <- function(...){
       out <- survey %>% select(!!!columns)
     }
     
+    
+    if(!is.null(fctr)){
+      if(fctr == TRUE){
+      out <- factor_cols(data=out, !!!columns)
+      } else {
+      out <- factor_cols(data=out, !!!columns, lvls = factor_lvls)
+      }
+    }
+    
    return(out) 
 }
-
-#get_factors(member_length)
+levelevel <- list(c(levels(as.factor(test$member_length)), "buns"))
+test <- get_factors(member_length, factor_lvls = TRUE)
